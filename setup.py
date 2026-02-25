8da96ef02fe60573785dd5da70f2e58be130fb7c
Gabriel Fabien-Ouellet <gabriel.fabien-ouellet@polymtl.ca>
2026-02-25 13:11:51 -0500

    Add setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="E-TRT",
+    version="0.1.0",
+    author="Clarissa Szabo-Som and Gabriel Fabien-Ouellet",
+    description="Electrical Thermal Response Test (E-TRT) simulation and inversion",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/E-TRT",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy>=1.26.0",
+        "scipy>=1.16.0",
+        "matplotlib>=3.10.0",
+        "IPython>=9.0.0",
+        "jupyter>=1.1.0",
+        "notebook>=7.4.0",
+        "discretize>=0.12.0",
+        "simpeg>=0.25.0",
+        "torch>=2.2.0",
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.2.0",
+            "pytest-cov>=2.12.0",
+            "black>=21.5b0",
+            "flake8>=3.9.0",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+    ],
+)
+
